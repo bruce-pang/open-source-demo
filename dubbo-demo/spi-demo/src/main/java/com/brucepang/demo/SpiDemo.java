17d857d9abbb2fd41a22c1e056613b2e513b5b49
brucepang <brucepang0618@gmail.com>
2024-07-18 23:41:21 +0800

    [dubbo-demo]: 获取Protocol的ExtensionLoader

--- dubbo-demo/spi-demo/src/main/java/com/brucepang/demo/SpiDemo.java
+++ dubbo-demo/spi-demo/src/main/java/com/brucepang/demo/SpiDemo.java
@@ -1,6 +1,7 @@
 package com.brucepang.demo;
 
 import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.rpc.Protocol;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
 import org.apache.dubbo.rpc.model.ApplicationModel;
 
@@ -16,13 +17,19 @@ public class SpiDemo {
         ExtensionLoader<LoadBalance> balanceExtensionLoader = ApplicationModel.defaultModel()
                 .getExtensionLoader(LoadBalance.class) // 获取筛选范围
                 ;
-        // 获取支持的所有的扩展点
+        // 获取支持的所有关于LoadBalance的扩展点
         System.out.println(balanceExtensionLoader.getSupportedExtensions());
 
-        // 获取指定的扩展点
+        // 获取指定的LoadBalance的扩展点
         LoadBalance loadBalance = ApplicationModel.defaultModel()
                 .getExtensionLoader(LoadBalance.class) // 获取筛选范围
                 .getExtension("brucerandom"); // 获取指定的扩展点
         System.out.println(loadBalance);
+
+        // 获取Protocol的ExtensionLoader
+        Protocol dubbo = ApplicationModel.defaultModel()
+                .getExtensionLoader(Protocol.class) // 获取筛选范围
+                .getExtension("dubbo"); // 获取指定的协议扩展点
+        System.out.println(dubbo);
     }
 }
